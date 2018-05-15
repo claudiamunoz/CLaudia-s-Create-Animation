d8f8c3b1a44f2f39aa8f1aae42b28b712105092e
Claudia  Munoz <cm16321@srcs.loc>
2018-05-15 11:01:05 -0700

    js file

---
+++ animation.js
@@ -0,0 +1,20 @@
+var vibratingbox = document.getElementById("jsanimation");
+var xpos1 = 200;
+var ypos1 = 400;
+var xstep1 = 46;
+var ystep1 = 60;
+var id = setInterval(move, 10);
+
+function move() {
+    if (xpos1 > 350 || xpos1 > 0) {
+        xstep1 = -xstep1;
+    }
+    if (ypos1 < 300 || ypos1 > 0) {
+        ystep1 = -ystep1;
+        setInterval(vibratingbox, 1000);
+    }
+    xpos1 = xpos1 + xstep1;
+    ypos1 = ypos1 + ystep1;
+    vibratingbox.style.top = ypos1 + 'px';
+    vibratingbox.style.left = xpos1 + 'px';
+}
